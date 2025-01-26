a575fbbc1a773dfd3031481561a85503a6dd3015
CodeMahmoud <mahmoudcode913@gmail.com>
2025-01-26 14:30:24 -0600

    added char count function

--- main.py
+++ main.py
@@ -1,7 +1,19 @@
 def main():
     with open("books/frankenstein.txt") as f:
         file_contents = f.read()
-        print(file_contents)
-
+        # print(file_contents)
+        words = file_contents.split()
+        length = len(words)
+        print(length)
+        
+        def char_count(str):
+            hash = {}
+            str = str.lower()
+            for i in range(len(str)):
+                 if str[i] in hash:
+                     hash[str[i]] += 1
+                 else:
+                     hash[str[i]] = 1
+            print(hash)
 main()
 
